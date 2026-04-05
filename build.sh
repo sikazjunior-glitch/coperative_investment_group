a62b4aca88fd8a041dfeb008b069ca76dcdefc0a
Sikazjunior <sikazjunior@gmail.com>
2026-04-05 03:02:23 +0200

    Added automatic superuser creation

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# NEW: Automatically create the admin using our Render Environment Variables
+python manage.py createsuperuser --noinput || true
